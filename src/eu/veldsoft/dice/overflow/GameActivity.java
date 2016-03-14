6f59492ba11ed991212e3102fce36fecefa3ec3f
Diana Dyulgerova <dianadyulgerova10@gmail.com>
2016-03-14 16:54:53 +0200

    UI was developed.

---
+++ src/eu/veldsoft/dice/overflow/GameActivity.java
@@ -0,0 +1,39 @@
+package eu.veldsoft.dice.overflow;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class GameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.game_option_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.new_game:
+			break;
+		case R.id.help:
+			startActivity(new Intent(GameActivity.this, HelpActivity.class));
+			break;
+		case R.id.about:
+			startActivity(new Intent(GameActivity.this, AboutActivity.class));
+			break;
+		}
+		return true;
+	}
+}
